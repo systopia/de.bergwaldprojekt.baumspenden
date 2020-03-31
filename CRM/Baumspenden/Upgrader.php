6dfc19c0926bc436fc8e37fb37ed0c4d34944a13
Jens Schuppe <schuppe@systopia.de>
2020-03-31 13:34:49 +0200

    (wip) Add custom fields for amount and unit price

---
+++ CRM/Baumspenden/Upgrader.php
@@ -0,0 +1,12 @@
+<?php
+
+use CRM_Baumspenden_ExtensionUtil as E;
+
+/**
+ * Class CRM_Baumspenden_Upgrader
+ */
+class CRM_Baumspenden_Upgrader extends CRM_Baumspenden_Upgrader_Base
+{
+    // TODO: Add custom fields (in install()? - extension was renamed!)
+    // TODO: Add default values for existing custom fields on contributions.
+}
