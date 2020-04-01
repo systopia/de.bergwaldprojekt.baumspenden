8c5cc57ac538529161d8110c540d8e5e575f016e
Jens Schuppe <schuppe@systopia.de>
2020-04-01 12:51:20 +0200

    Add copyright headers

--- api/v3/BWPBaumspende/Submit.php
+++ api/v3/BWPBaumspende/Submit.php
@@ -1,4 +1,17 @@
 <?php
+/*-------------------------------------------------------+
+| Bergwaldprojekt Baumspenden                            |
+| Copyright (C) 2020 SYSTOPIA                            |
++--------------------------------------------------------+
+| This program is released as free software under the    |
+| Affero GPL license. You can redistribute it and/or     |
+| modify it under the terms of this license which you    |
+| can read by viewing the included agpl.txt or online    |
+| at www.gnu.org/licenses/agpl.html. Removal of this     |
+| copyright header is strictly prohibited without        |
+| written permission from the original author(s).        |
++--------------------------------------------------------*/
+
 use CRM_Baumspenden_ExtensionUtil as E;
 
 /**
--- baumspenden.php
+++ baumspenden.php
@@ -1,4 +1,16 @@
 <?php
+/*-------------------------------------------------------+
+| Bergwaldprojekt Baumspenden                            |
+| Copyright (C) 2020 SYSTOPIA                            |
++--------------------------------------------------------+
+| This program is released as free software under the    |
+| Affero GPL license. You can redistribute it and/or     |
+| modify it under the terms of this license which you    |
+| can read by viewing the included agpl.txt or online    |
+| at www.gnu.org/licenses/agpl.html. Removal of this     |
+| copyright header is strictly prohibited without        |
+| written permission from the original author(s).        |
++--------------------------------------------------------*/
 
 require_once 'baumspenden.civix.php';
 use CRM_Baumspenden_ExtensionUtil as E;
